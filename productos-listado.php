3f5705f97533c01eaa3fcadab3cdb846457bf66f
David Winze <38311518+DWinze@users.noreply.github.com>
2020-01-29 12:08:38 +0100

    DAO
    dao de juanki

---
+++ productos-listado.php
@@ -0,0 +1,47 @@
+<?php
+
+require_once "_clases.php";
+require_once "_dao.php";
+
+$productos = DAO::productoObtenerTodos();
+
+?>
+
+
+
+<html>
+
+<head>
+    <meta charset="UTF-8">
+</head>
+
+<body>
+
+<table border="1">
+
+    <tr>
+        <th>Producto</th>
+        <th>Precio</th>
+        <th>Añadir</th>
+    </tr>
+
+    <?php foreach ($productos as $producto) { ?>
+        <tr>
+            <td>
+                <a href='producto-detalle.php?id=<?=$producto->getId()?>'><?=$producto->getNombre()?></a>
+
+            </td>
+            <td>
+                <a href='producto-detalle.php?id=<?=$producto->getId()?>'><?=$producto->generarPrecioFormateado()?></a>
+            </td>
+            <td>
+                <a href='carrito-añadir-producto.php?productoId=<?=$producto->getId()?>'>Al carrito</a>
+            </td>
+        </tr>
+    <?php } ?>
+
+</table>
+
+</body>
+
+</html>
