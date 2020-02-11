a4ad6ca375443c1ef9421db2c61d5265fc15c4aa
Sergio <sergiodiazm0001@gmail.com>
2020-02-11 11:07:44 +0100

    Inserción de datos en la BBDD

--- tienda.sql
+++ tienda.sql
@@ -2,13 +2,10 @@
 -- version 4.9.1
 -- https://www.phpmyadmin.net/
 --
--- Servidor: localhost
--- Tiempo de generación: 27-01-2020 a las 14:08:33
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 11-02-2020 a las 10:40:02
 -- Versión del servidor: 10.4.8-MariaDB
 -- Versión de PHP: 7.3.11
-DROP DATABASE IF EXISTS tienda;
-CREATE DATABASE tienda;
-USE tienda;
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -24,8 +21,6 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `tienda`
 --
-CREATE DATABASE IF NOT EXISTS `tienda` DEFAULT CHARACTER SET utf8 COLLATE utf8_spanish_ci;
-USE `tienda`;
 
 -- --------------------------------------------------------
 
@@ -33,7 +28,6 @@ USE `tienda`;
 -- Estructura de tabla para la tabla `cliente`
 --
 
-DROP TABLE IF EXISTS `cliente`;
 CREATE TABLE `cliente` (
   `id` int(11) NOT NULL,
   `email` varchar(60) COLLATE utf8_spanish_ci NOT NULL,
@@ -46,18 +40,14 @@ CREATE TABLE `cliente` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 --
--- Truncar tablas antes de insertar `cliente`
---
-
-TRUNCATE TABLE `cliente`;
---
 -- Volcado de datos para la tabla `cliente`
 --
 
 INSERT INTO `cliente` (`id`, `email`, `contrasenna`, `codigoCookie`, `nombre`, `direccion`, `telefono`, `registrado`) VALUES
 (1, 'jlopez@gmail.com', 'j', 'J4PTyau8zvK1EfrwLANAn7dT81tWRtyl', 'José', NULL, NULL, 0),
 (2, 'mgarcia@gmail.com', 'm', NULL, 'María', NULL, NULL, 0),
-(3, 'jfernandez@gmail.com', '1234', 'MPkkCyPr5filHF0VuWilH4v7J6zJuupy', 'Juanito', NULL, NULL, 0);
+(3, 'jfernandez@gmail.com', '1234', 'MPkkCyPr5filHF0VuWilH4v7J6zJuupy', 'Juanito', NULL, NULL, 0),
+(4, 'cliente@gmail.com', '1234', NULL, 'Alfonso', 'Avda. Real de Pinto', '621907133', 0);
 
 -- --------------------------------------------------------
 
@@ -65,7 +55,6 @@ INSERT INTO `cliente` (`id`, `email`, `contrasenna`, `codigoCookie`, `nombre`, `
 -- Estructura de tabla para la tabla `linea`
 --
 
-DROP TABLE IF EXISTS `linea`;
 CREATE TABLE `linea` (
   `pedido_id` int(11) NOT NULL,
   `producto_id` int(11) NOT NULL,
@@ -74,17 +63,23 @@ CREATE TABLE `linea` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 --
--- Truncar tablas antes de insertar `linea`
---
-
-TRUNCATE TABLE `linea`;
---
 -- Volcado de datos para la tabla `linea`
 --
 
 INSERT INTO `linea` (`pedido_id`, `producto_id`, `unidades`, `precioUnitario`) VALUES
 (1, 1, 3, NULL),
-(1, 2, 1, NULL);
+(1, 2, 1, NULL),
+(1, 3, 3, NULL),
+(1, 10, 5, NULL),
+(2, 9, 2, NULL),
+(2, 17, 1, NULL),
+(3, 5, 6, NULL),
+(3, 7, 8, NULL),
+(3, 8, 1, NULL),
+(3, 12, 1, NULL),
+(4, 16, 1, NULL),
+(4, 19, 1, NULL),
+(4, 20, 2, NULL);
 
 -- --------------------------------------------------------
 
@@ -92,7 +87,6 @@ INSERT INTO `linea` (`pedido_id`, `producto_id`, `unidades`, `precioUnitario`) V
 -- Estructura de tabla para la tabla `pedido`
 --
 
-DROP TABLE IF EXISTS `pedido`;
 CREATE TABLE `pedido` (
   `id` int(11) NOT NULL,
   `cliente_id` int(11) NOT NULL,
@@ -101,16 +95,15 @@ CREATE TABLE `pedido` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 --
--- Truncar tablas antes de insertar `pedido`
---
-
-TRUNCATE TABLE `pedido`;
---
 -- Volcado de datos para la tabla `pedido`
 --
 
 INSERT INTO `pedido` (`id`, `cliente_id`, `direccionEnvio`, `fechaConfirmacion`) VALUES
-(1, 1, NULL, NULL);
+(1, 1, 'Calle Gabriel y Galán, 3, Getafe, Madrid', '2020-02-03 14:25:20'),
+(2, 2, 'Calle Arquitectos, 14, Getafe, Madrid', '2019-11-08 21:51:43'),
+(3, 3, 'Calle Pedro Almodóvar, 3, Getafe, Madrid', '2020-01-30 09:13:39'),
+(4, 4, 'Calle los Arcos, 30, Getafe, Madrid', '2020-02-02 16:41:23'),
+(5, 2, 'Calle Teruel, 13, Getafe, Madrid', '2019-12-26 04:44:13');
 
 -- --------------------------------------------------------
 
@@ -118,7 +111,6 @@ INSERT INTO `pedido` (`id`, `cliente_id`, `direccionEnvio`, `fechaConfirmacion`)
 -- Estructura de tabla para la tabla `producto`
 --
 
-DROP TABLE IF EXISTS `producto`;
 CREATE TABLE `producto` (
   `id` int(11) NOT NULL,
   `nombre` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
@@ -127,17 +119,30 @@ CREATE TABLE `producto` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 --
--- Truncar tablas antes de insertar `producto`
---
-
-TRUNCATE TABLE `producto`;
---
 -- Volcado de datos para la tabla `producto`
 --
 
 INSERT INTO `producto` (`id`, `nombre`, `descripcion`, `precio`) VALUES
 (1, 'Cafetera', 'Modelo 5000, con doble filtro de partículas.', '9.00'),
-(2, 'Roomba', 'Suba y baja escaleras, último modelo.', '11.67');
+(2, 'Roomba', 'Sube y baja escaleras, último modelo.', '150.00'),
+(3, 'Atun claro en aceite oliva', 'Pack de 6 latas', '4.00'),
+(4, 'Fuet espetec extra', 'Pieza de 240gr', '2.05'),
+(5, 'Tomate frito', 'Pack de 3 botes', '1.01'),
+(6, 'Bacon taquitos', 'Pack 2x125gr', '1.95'),
+(7, 'Macarron pasta', 'Paquete de 1kg', '0.76'),
+(8, 'Nata ligera para cocinar', 'Pack de 3 botes, 200ml/unidad', '1.32'),
+(9, 'Jamón cocido extra', 'Lonchas finas - Pack 2x225gr', '3.08'),
+(10, 'Caldo líquido de pollo', 'Pack 2x1L', '1.64'),
+(11, 'Tomate natural triturado', 'Bote de 400gr', '0.45'),
+(12, 'Arroz redondo', 'Paquete de 1kg', '0.79'),
+(13, 'Pan de molde', 'Blanco sin corteza - 450gr', '1.09'),
+(14, 'Aceite girasol', 'Botella de 1L', '0.99'),
+(15, 'Queso rallado', 'Paquete de tiras de mozzarella - 200gr', '1.20'),
+(16, 'Sony a7sii', 'Cuerpo de cámara mirrorless gama ISO', '2049.50'),
+(17, 'Nintendo Switch', 'Consola color Azul Neón/Rojo Neón (Modelo 2019)', '315.99'),
+(18, 'Echo Dot (3.ª generación)', 'Altavoz inteligente con Alexa, tela de color antracita', '40.01'),
+(19, 'Diablo X-One Gaming', 'Silla de Oficina Diseño Ergonomico Mecanismo de Inclinación Cojin Lumbar y Almohada Cuero Sintético (Negro-Rojo)', '159.99'),
+(20, 'Teclado Gaming', 'TedGem Teclado Gaming, Teclado USB, Teclado Gaming PS4 LED Retroiluminado con Cable USB, Teclado para PC / Laptop / PS4 / Xbox One (Teclados Español, Negro)', '159.99');
 
 --
 -- Índices para tablas volcadas
@@ -177,19 +182,19 @@ ALTER TABLE `producto`
 -- AUTO_INCREMENT de la tabla `cliente`
 --
 ALTER TABLE `cliente`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `pedido`
 --
 ALTER TABLE `pedido`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT de la tabla `producto`
 --
 ALTER TABLE `producto`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- Restricciones para tablas volcadas
